e30df5cdf250957d7c93a1809e3d4cbdcc8b8645
Monica Itzel Romero Calva <240498@utxicotepec.edu.mx>
2026-02-05 14:54:11 -0600

    update: se actualizaron las tarjetas para que no sean estaticas

--- my-react-app/src/ContenedorTarjeta.jsx
+++ my-react-app/src/ContenedorTarjeta.jsx
@@ -1,23 +1,27 @@
 import './ContenedorTarjetas.css';
-import imgTarjeta from "./assets/Recursos/pc.png";
+import imgTarjeta1 from "./assets/Recursos/pc.png";
+import imgTarjeta2 from "./assets/Recursos/pc2.png";
+import imgTarjeta3 from "./assets/Recursos/pc3.png";
+import imgTarjeta4 from "./assets/Recursos/pc4.png";
+
 
 function ContenedorTarjeta() {
     return (
         <div className="ContenedorTarjeta">
-            <Tarjeta />
-            <Tarjeta />
-            <Tarjeta />
-            <Tarjeta /> 
+            <Tarjeta titulo="Marcianware" descripcion="Una cosito" img={imgTarjeta1}/>
+            <Tarjeta titulo="Alienware" descripcion="Una pc de alta gama" img={imgTarjeta2}/>
+            <Tarjeta titulo="Olaware" descripcion="Una copia de la pc de alta gama" img={imgTarjeta3}/>
+            <Tarjeta titulo="Cosoware" descripcion="Ni idea bro" img={imgTarjeta4}/> 
         </div>
     );
 }
 
-function Tarjeta(){
+function Tarjeta(props){
     return(
         <div className="Tarjeta">
-            <img src={imgTarjeta} alt="Imagen de la tarjeta"/>
-            <h3>Marcianoware</h3>
-            <p>una lokura de pc</p>
+            <img src={props.img} alt="Imagen de la tarjeta"/>
+            <h3>{props.titulo}</h3>
+            <p>{props.descripcion}</p>
             <a href="#">Ver más</a>
             </div>
     )
